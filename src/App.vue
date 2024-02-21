36c10292d08aec7832131b2c7cd99aeb842302d7
hhk-png <fangluoabc@163.com>
2024-02-21 17:43:40 +0800

    some trivial

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { nextTick, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const scrollContainerDOM = ref<HTMLElement>();
 /**
  * 滑动：
  *  1. scrollIntoView() 方法可以自动回到上次浏览的位置。
  *  2. 使用window.scrollTo()，但是该方法需要使用定时器多次调用才能实现滑动效果。
+ *  3. 使用vuerouter的scrollBehavior 方法，该方法需要在路由切换时才会触发。
  * 
 */
 onMounted(() => {
